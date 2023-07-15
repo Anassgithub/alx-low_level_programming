353ece1e7f65065bf6cf9240da787df8593aa8d7
Anassgithub <anasselmoussaoui97@gmail.com>
2023-07-14 18:15:52 -0700

    4-puts.c 5-printf.c 6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * A C program that print with put funtion
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	Return(0);
+}
