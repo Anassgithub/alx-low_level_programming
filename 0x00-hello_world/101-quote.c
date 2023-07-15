c9f912fb11dba1dca755073bb5b36215bc3c970d
Anassgithub <anasselmoussaoui97@gmail.com>
2023-07-15 11:56:30 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<unistd.h>
+
+/**
+ * main - entry point
+ *
+ * A C program that print quot using write function
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
