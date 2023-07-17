ef560dee92a1cb1bda230f1647ed08690305107e
Anassgithub <anasselmoussaoui97@gmail.com>
2023-07-17 10:09:12 -0700

    Print combinations of numbers using putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print combinations of numbers
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
