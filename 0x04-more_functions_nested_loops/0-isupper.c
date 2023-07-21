8ca6e1ddcad1e8a6462e25e3adb111e8e7dd1870
Anassgithub <anasselmoussaoui97@gmail.com>
2023-07-21 12:58:50 -0700

    function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: function parameter
+ *
+ * Return: 1 if it is uppercase otherwise 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
