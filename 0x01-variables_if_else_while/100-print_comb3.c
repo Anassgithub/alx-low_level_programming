38aab82c592369f478c4802e6095087bb5d11c50
Anassgithub <anasselmoussaoui97@gmail.com>
2023-07-22 15:48:30 -0700

    delete 0x04-more_functions_nested_loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Points
+ *
+ * Description: Print combinations of two digits using putchar
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int d1 = 0;
+	int d2;
+
+	while (d1 
+				
